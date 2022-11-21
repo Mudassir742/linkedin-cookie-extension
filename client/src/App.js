0613984edf42fb35b6ee97ffac0761034b916efd
Mudassir742 <mudassirwaheed242@gmail.com>
2022-11-21 16:45:30 +0500

    get company detail

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,21 @@
 import { useState, useEffect } from "react";
-//icons
-import { AiFillCar as Car, AiFillAccountBook as Book } from "react-icons/ai";
-import { BsBicycle as Cycle } from "react-icons/bs";
-import { FaHelicopter as Choper } from "react-icons/fa";
-import { GiAnglerFish as Fish } from "react-icons/gi";
-
-import Widgets from "./components/Widgets";
-
 import axios from "axios";
 
 import "./App.css";
 
 function App() {
+  const [emplyeCount, setEmployeCount] = useState("");
+
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await axios.get(
-          "https://www.linkedin.com/voyager/api/entities/companies/2652230",
-          {
-            headers: {
-              "csrf-token":"ajax:7205970595350190655",
-              Cookie:
-                "li_at=AQEDAT9XPdkFy5DxAAABhJZsuEIAAAGEunk8Qk0AS_9D3llQ-HI01sZVN7wcrZBuvLs0ZLj7XraZxPLDYRJl9_vP1UYbV1I6QHVK-LnFpzxHeoMYu4fqY39f-pgtFjpZ-Mo-dvnlP2jD1Z8tOnqhYj01;JSESSIONID=ajax:7205970595350190655;",
-            },
-          }
-        );
-        console.log(response)
+        const response = await axios.post("http://localhost:5000/get-data", {
+          liAt: "AQEDAT9XPdkFy5DxAAABhJZsuEIAAAGEunk8Qk0AS_9D3llQ-HI01sZVN7wcrZBuvLs0ZLj7XraZxPLDYRJl9_vP1UYbV1I6QHVK-LnFpzxHeoMYu4fqY39f-pgtFjpZ-Mo-dvnlP2jD1Z8tOnqhYj01",
+          token: "ajax:7205970595350190655",
+          ssid: "ajax:7205970595350190655",
+        });
+        console.log(response);
+        setEmployeCount(response.data?.emplyeCount)
       } catch (error) {
         console.log(error);
       }
@@ -33,36 +23,8 @@ function App() {
 
     getData();
   }, []);
-  // const [carWidget, setCarWidget] = useState([]);
-  // const [fishWidget, setFishWidget] = useState([]);
-  // const [cycleWidget, setCycleWidget] = useState([]);
-  // const [bookWidget, setBookWidget] = useState([]);
-  // const [choperWidget, setChoperWidget] = useState([]);
 
-  return <></>;
-
-  //return (
-  // <div className="App">
-  //   {/*Car Widget*/}
-  //   <Widgets Icon={Car} widget={carWidget} setWidget={setCarWidget} />
-
-  //   {/*Fish Widget*/}
-  //   <Widgets Icon={Fish} widget={fishWidget} setWidget={setFishWidget} />
-
-  //   {/*Cycle Widget*/}
-  //   <Widgets Icon={Cycle} widget={cycleWidget} setWidget={setCycleWidget} />
-
-  //   {/*Book Widget*/}
-  //   <Widgets Icon={Book} widget={bookWidget} setWidget={setBookWidget} />
-
-  //   {/*Choper Widget*/}
-  //   <Widgets
-  //     Icon={Choper}
-  //     widget={choperWidget}
-  //     setWidget={setChoperWidget}
-  //   />
-  // </div>
-  // );
+  return <>{emplyeCount && <h1>{emplyeCount}</h1>}</>;
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -20,52 +20,29 @@ app.post("/check", (req, res) => {
   return res.status(200).json({ message: "all good" });
 });
 
-app.get("/get-data",async(req,res)=>{
+app.post("/get-data",async(req,res)=>{
   try{
-  //   const options = {
-  //     hostname: 'https://www.linkedin.com',
-  //     path: '/voyager/api/entities/companies/2652230',
-  //     method: 'GET',
-  //     headers: {
-  //       Cookies:
-  //         "li_at:u4fqY39f-pgtFjpZ-Mo-dvnlP2jD1Z8tOnqhYj01;JSESSIONID:ajax:7205970595350190655;csrf-token:ajax:7205970595350190655;",
-  //     },
-  //   }
-  //   const resp = http.request(options, (response) => {
-  //     let data = ''
-       
-  //     response.on('data', (chunk) => {
-  //         data += chunk;
-  //     });
-      
-  //     // Ending the response 
-  //     response.on('end', () => {
-  //         console.log('Body:', JSON.parse(data))
-  //     });
-         
-  // }).on("error", (err) => {
-  //     console.log("Error: ", err)
-  // }).end()
-  
+  const {liAt,token,ssid} = req.body
+    console.log(req.body)
   const response = await axios.get(
     "https://www.linkedin.com/voyager/api/entities/companies/2652230",
     {
       headers: {
-        "csrf-token":"ajax:7205970595350190655",
+        "csrf-token":`${token}`,
         Cookie:
-          "li_at=AQEDAT9XPdkFy5DxAAABhJZsuEIAAAGEunk8Qk0AS_9D3llQ-HI01sZVN7wcrZBuvLs0ZLj7XraZxPLDYRJl9_vP1UYbV1I6QHVK-LnFpzxHeoMYu4fqY39f-pgtFjpZ-Mo-dvnlP2jD1Z8tOnqhYj01;JSESSIONID=ajax:7205970595350190655;",
+          `li_at=${liAt};JSESSIONID=${ssid};`,
       },
       
     },
     {withCredentials: true}
   );
-  console.log(response)
+  console.log(response.data)
 
-  res.status(200).json({message:"ok"})
+  res.status(200).json({emplyeCount:response.data.employeeCountRange})
   
   }catch(error){
     console.log(error)
-    res.status(500).json({message:"server error"})
+    res.status(500).json({error:"server error"})
   }
 })
 
